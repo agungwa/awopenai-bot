5b417ccca63ceb643ca30443525b40d40963279e
agung <agung.widhiatmojo@gmail.com>
2022-12-10 23:20:23 +0700

    git init project

---
+++ index.js
@@ -0,0 +1,30 @@
+import { ChatGPTAPI } from "chatgpt";
+import { Telegraf } from "telegraf";
+
+async function chat() {
+    const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
+    const api = new ChatGPTAPI({
+        sessionToken: process.env.CHAT_TOKEN
+    })
+    
+    await api.ensureAuth()
+
+    bot.start((ctx) => ctx.reply('Welcome to AW open AI'));
+    bot.help((ctx) => ctx.reply('Type : Help me please!'));
+    bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+    bot.on('text', async (ctx) => {
+        const response = await api.sendMessage( ctx.message?.text );
+        bot.telegram.sendMessage(ctx.chat.id, response);
+
+      })
+
+
+    bot.launch();
+    // Enable graceful stop
+    process.once('SIGINT', () => bot.stop('SIGINT'));
+    process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+}
+
+chat()
+
