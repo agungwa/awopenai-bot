318a25b69146eb467eeb478502de86fd4ae4c849
agung <agung.widhiatmojo@gmail.com>
2022-12-11 00:01:37 +0700

    add chat logs

--- index.js
+++ index.js
@@ -3,28 +3,24 @@ import { Telegraf } from "telegraf";
 import dotenv from 'dotenv'
 dotenv.config()
 
-async function chat() {
-    const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
-    const api = new ChatGPTAPI({
-        sessionToken: process.env.CHAT_TOKEN
-    })
-    
+const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
+const api = new ChatGPTAPI({
+    sessionToken: process.env.CHAT_TOKEN
+})
+
+bot.start((ctx) => ctx.reply('Welcome to AW open AI'));
+bot.help((ctx) => ctx.reply('Type : Help me please!'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.on('text', async (ctx) => {
     await api.ensureAuth()
+    const response = await api.sendMessage( ctx.message?.text );
+    console.log('MESSAGE: ',ctx.message?.text)
+    console.log('RESPONSE: ',response)
+    bot.telegram.sendMessage(ctx.chat.id, response);
+    })
 
-    bot.start((ctx) => ctx.reply('Welcome to AW open AI'));
-    bot.help((ctx) => ctx.reply('Type : Help me please!'));
-    bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-    bot.on('text', async (ctx) => {
-        const response = await api.sendMessage( ctx.message?.text );
-        bot.telegram.sendMessage(ctx.chat.id, response);
-      })
-
-    bot.launch();
-    // Enable graceful stop
-    process.once('SIGINT', () => bot.stop('SIGINT'));
-    process.once('SIGTERM', () => bot.stop('SIGTERM'));
-
-}
-
-chat()
+bot.launch();
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
